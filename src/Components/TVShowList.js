cf1d79d0c2a42986cbacd8dfce6fd4787ca1417a
clarkj99 <clark@stontech.net>
2019-12-16 15:07:46 -0500

    Done.

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import {Grid} from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import TVShow from './TVShow';
 
 class TVShowList extends Component {
 
   mapAllShows = () => {
-    if (!!props.searchTerm){
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
+
+    if (!!this.props.searchTerm && !!this.props.filterRating) {
+      return this.props.shows.map((s) => {
+        if (s.name.toLowerCase().includes(this.props.searchTerm) && s.rating.average >= this.props.filterRating) {
+          return (<TVShow show={s} key={s.id} selectShow={this.props.selectShow} />)
+        } else {
+          return null
         }
       })
+    } else if (!!this.props.searchTerm) {
+      return this.props.shows.map((s) => {
+        if (s.name.toLowerCase().includes(this.props.searchTerm)) {
+          return (<TVShow show={s} key={s.id} selectShow={this.props.selectShow} />)
+        } else {
+          return null
+        }
+      })
+    } else if (!!this.props.filterRating) {
+      return this.props.shows.map((s) => {
+        if (s.rating.average >= this.props.filterRating) {
+          return (<TVShow show={s} key={s.id} selectShow={this.props.selectShow} />)
+        } else {
+          return null
+        }
+      })
+    } else {
+
+      return this.props.shows.map((s) => <TVShow show={s} key={s.id} selectShow={this.props.selectShow} />)
     }
-    return props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={props.selectShow}/>)
   }
+
+
 
   render() {
     return (
